f238db2550144824ddb1e0db73773136e50633fc
KI8YK8 <k***@gmail.com>
2025-10-05 20:53:14 +0545

    adds basic react slider

--- src/games/range/range.jsx
+++ src/games/range/range.jsx
@@ -1,9 +1,71 @@
+import { useState } from "react";
+import { Range } from "react-range";
+
 export default function RangeGame() {
+	// [winner, min guess, max guess]
+	const [range_values, set_range_values] = useState([0, 10, 90]);
 	return (
 		<section>
 			<header>
 				<h3>Range Game</h3>
 			</header>
+			<main>
+				<Range
+					values={range_values}
+					step={1}
+					min={0}
+					max={100}
+					allowOverlap
+					onChange={set_range_values}
+					renderTrack={({ props, children }) => {
+						const [p0, p1, p2] = range_values;
+
+						return (
+							<div
+								{...props}
+								style={{
+									...props.style,
+									height: "6px",
+									width: "100%",
+									background: "red",
+									position: "relative",
+								}}
+							>
+								{/*Between P1 and P2*/}
+								<div
+									style={{
+										position: "absolute",
+										left: `${p1}%`,
+										width: `${p2 - p1}%`,
+										height: "100%",
+										background: "green",
+									}}
+								/>
+								{children}
+							</div>
+						);
+					}}
+					renderThumb={({ props, index }) => {
+						const { key, ...rest_props } = props;
+
+						return (
+							<div
+								key={key}
+								{...rest_props}
+								style={{
+									...rest_props.style,
+									height: "16px",
+									width: "16px",
+									borderRadius: "50%",
+									background: index === 0 ? "black" : "grey",
+									zIndex: index === 0 ? 10 : 5,
+								}}
+							/>
+						);
+					}}
+				/>
+			</main>
+			<p>{range_values}</p>
 		</section>
 	);
 }
